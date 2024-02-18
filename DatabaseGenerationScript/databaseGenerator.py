0d5f3022e6f836a14a20d0cd44dd48309ce1a5a0
jnuccitelli <jnuccitelli1@gmail.com>
2024-02-17 22:57:25 -0600

    connection working

--- DatabaseGenerationScript/databaseGenerator.py
+++ DatabaseGenerationScript/databaseGenerator.py
@@ -1,10 +1,19 @@
 import psycopg2 as psy
 
+#basically a wrapper class bc why not 
+class DbGenerator: 
 
-class DbGenerator:    
+    conn = None
+    cur = None
 
-    def Connect(self,databasename,username):
-        psy.connect("dbname=" + databasename + " user=" + username)
-        return
+    def Connect(self,databasename,username,password,host):
+        try:
+            self.conn = psy.connect("dbname=" + databasename + " user=" + username + " password=" + password + " host=" + host) 
+            self.cur = self.conn.cursor()
+            return True
+        except:
+            return False
 
+    def __init__(self):
+        pass
     
--- DatabaseGenerationScript/main.py
+++ DatabaseGenerationScript/main.py
@@ -1,12 +1,13 @@
 import databaseGenerator 
 
+
 def Main():
 
+
     db = databaseGenerator.DbGenerator()
-    
-    if not db.Connect("dbname","username"):
-        raise ConnectionError("Failed to connect to the database")
-        return -1
+    if not db.Connect("csce315_902_01_db","csce315_902_01_user","team21","csce-315-db.engr.tamu.edu"):
+        raise ConnectionError("woah this means you are probably not on wifi, do better")
+
     
     return
 
