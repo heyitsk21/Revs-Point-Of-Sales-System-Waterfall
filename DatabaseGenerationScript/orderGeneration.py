1a774bc49c4783d841bfa90e758f4b456f80416c
jnuccitelli <jnuccitelli1@gmail.com>
2024-02-19 15:21:55 -0600

    Order generation skeleton code

---
+++ DatabaseGenerationScript/orderGeneration.py
@@ -0,0 +1,16 @@
+import random
+import csv
+import random
+#add more 
+
+
+NAMEPOOL = ["Josh","Jacob","Bart","Tom","Sydney","Ashton","Claire","Noah","James","Charles"
+            "Amelia","Liam","Mia","Luca","Apollo","Hazel","Grover","George","John","Thomas","Marilyn"
+            "Madison"]
+
+
+class OrderGenerator:
+    def CreateOrder(self):
+        pass
+    def __init__(self):
+        pass
