695187eccded7b7aec1da7cf326ca0626711b49f
Joshua Robert <joshdrobert@tamu.edu>
2024-02-22 13:48:20 -0600

    GUI updated with passwords in ignored file for security

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,98 +1,96 @@
-// TO DO:import java.sql.*;
+import java.sql.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-/*
-  TODO:
-  1) Change credentials for your own team's database
-  2) Change SQL command to a relevant query that retrieves a small amount of data
-  3) Create a JTextArea object using the queried data
-  4) Add the new object to the JPanel p
-*/
+import java.io.*;
 
 public class GUI extends JFrame implements ActionListener {
     static JFrame f;
 
-    public static void main(String[] args)
-    {
-      //Building the connection
-      Connection conn = null;
-      //TODO STEP 1 (see line 7)
-      String database_name = "csce315_902_01_db";
-      String database_user = "csce315_902_01_user";
-      String database_password = "";
-      String database_url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", database_name);
-      try {
-        conn = DriverManager.getConnection(database_url, database_user, database_password);
-      } catch (Exception e) {
-        e.printStackTrace();
-        System.err.println(e.getClass().getName()+": "+e.getMessage());
-        System.exit(0);
-      }
-      JOptionPane.showMessageDialog(null,"Opened database successfully");
-
-      String name = "";
-      String price = "";
-      try{
-        //create a statement object
-        Statement stmt = conn.createStatement();
-        //create a SQL statement
-        //TODO Step 2 (see line 8)
-        String sqlStatement = "SELECT itemname, price FROM menuitems;";
-        //send statement to DBMS
-        ResultSet result = stmt.executeQuery(sqlStatement);
-        while (result.next()) {
-          // TODO you probably need to change the column name tat you are retrieving
-          //      this command gets the data from the "name" attribute
-          name += result.getString("itemName")+" ";
-          name += result.getString("price")+"\n";
+    public static void main(String[] args) {
+        // Read credentials from login.txt
+        String database_user = "";
+        String database_password = "";
+        try (BufferedReader br = new BufferedReader(new FileReader("login.txt"))) {
+            database_user = br.readLine();
+            database_password = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
-      } catch (Exception e){
-        JOptionPane.showMessageDialog(null,"Error accessing Database.");
-      }
-      // create a new frame
-      f = new JFrame("DB GUI");
 
-      // create a object
-      GUI s = new GUI();
+        //Building the connection
+        Connection conn = null;
+        String database_name = "csce315_902_01_db";
+        String database_url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", database_name);
+        try {
+            conn = DriverManager.getConnection(database_url, database_user, database_password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        JOptionPane.showMessageDialog(null, "Opened database successfully");
 
-      // create a panel
-      JPanel p = new JPanel();
+        String name = "";
+        String price = "";
+        try {
+            //create a statement object
+            Statement stmt = conn.createStatement();
+            //create a SQL statement
+            String sqlStatement = "SELECT itemname, price FROM menuitems;";
+            //send statement to DBMS
+            ResultSet result = stmt.executeQuery(sqlStatement);
+            while (result.next()) {
+                // TODO you probably need to change the column name tat you are retrieving
+                //      this command gets the data from the "name" attribute
+                name += result.getString("itemName") + " ";
+                name += result.getString("price") + "\n";
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error accessing Database.");
+        }
+        // create a new frame
+        f = new JFrame("DB GUI");
 
-      JButton b = new JButton("Close");
+        // create a object
+        GUI s = new GUI();
 
-      // add actionlistener to button
-      b.addActionListener(s);
- 
-      //TODO Step 3 (see line 9) -- Create a JTextArea object using the queried data
-      JTextArea textArea = new JTextArea(name + "\n" + price);
+        // create a panel
+        JPanel p = new JPanel();
 
-      //TODO Step 4 (see line 10) -- Add the new object to the JPanel p
-      p.add(textArea);
+        JButton b = new JButton("Close");
 
-      // add button to panel
-      p.add(b);
+        // add actionlistener to button
+        b.addActionListener(s);
 
-      // add panel to frame
-      f.add(p);
+        // Create a JTextArea object using the queried data
+        JTextArea textArea = new JTextArea(name + "\n" + price);
 
-      // set the size of frame
-      f.setSize(400, 400);
+        // Add the new object to the JPanel p
+        p.add(textArea);
 
-      f.setVisible(true);
+        // add button to panel
+        p.add(b);
 
-      //closing the connection
-      try {
-        conn.close();
-        JOptionPane.showMessageDialog(null,"Connection Closed.");
-      } catch(Exception e) {
-        JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-      }
+        // add panel to frame
+        f.add(p);
+
+        // set the size of frame
+        f.setSize(400, 400);
+
+        f.setVisible(true);
+
+        //closing the connection
+        try {
+            conn.close();
+            JOptionPane.showMessageDialog(null, "Connection Closed.");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Connection NOT Closed.");
+        }
     }
 
     // if button is pressed
-    public void actionPerformed(ActionEvent e)
-    {
+    public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
         if (s.equals("Close")) {
             f.dispose();
