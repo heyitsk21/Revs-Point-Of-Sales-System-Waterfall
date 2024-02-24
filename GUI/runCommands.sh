830f92c63e1ac854be9add7a660ad7cca2a141dc
Joshua Robert <joshdrobert@tamu.edu>
2024-02-24 16:15:42 -0600

    Added employee commands and mac compatibility

---
+++ GUI/runCommands.sh
@@ -0,0 +1,10 @@
+javac -cp bin -d bin sqlCommands/*.java
+javac -cp bin -d bin Manager/Panels/*.java
+javac -cp bin -d bin Manager/*.java
+javac -cp bin -d bin Employee/EmployeeEditOrderPanel.java
+javac -cp bin -d bin Employee/EmployeeSubmitOrderPanel.java
+javac -cp bin -d bin Employee/*.java
+javac -cp bin -d bin Database.java
+javac -cp bin -d bin LogInGUI.java
+javac -cp bin -d bin GUI.java
+java -cp "bin:postgresql-42.2.8.jar" GUI
