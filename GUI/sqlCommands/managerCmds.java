c80c94adf5a53441f0bda39ad75a8f1dc7c81b83
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2024-02-27 11:58:18 -0600

    Final bug fixes

--- GUI/sqlCommands/managerCmds.java
+++ GUI/sqlCommands/managerCmds.java
@@ -94,6 +94,7 @@ public class managerCmds {
 
     public sqlObjects.MenuItemIngredients getMenuItemIngredients(int menuItemID){
         try{
+            System.out.println(menuItemID);
             int size = 0;
             PreparedStatement prep;
             ResultSet allMenuItemIngredients;
@@ -126,7 +127,7 @@ public class managerCmds {
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
-        return null;
+        return new sqlObjects.MenuItemIngredients(new int[0],new String[0]);
     }
 
     public sqlObjects.OrderList getOrders() {
